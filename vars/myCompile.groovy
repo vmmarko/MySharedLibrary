885a3bea988bad88db8164118b8bda25ca6e2e26
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 18:03:34 +0200

    compile echo

--- vars/myCompile.groovy
+++ vars/myCompile.groovy
@@ -9,10 +9,13 @@ node {
 
 		if (isUnix()){
 			sh '$MAVEN_HOME/mvn compile'
+			echo "Compile Unix"
 		} else {
 			bat(/call mvn clean compile/)
+			echo "Compile Win"
 		}
 	}
+	echo "Compile end"
 }
 
 
