c67e1320aaf6e8ea993052b2118d75bb9c1be430
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 14:10:55 +0200

    fixTest

--- vars/myTest.groovy
+++ vars/myTest.groovy
@@ -7,9 +7,8 @@ def call() {
 	
 	if (isUnix()){
 		sh '$MAVEN_HOME/mvn surefire-report:report'
-		//junit '**/target/surefire-reports/TEST-.xml'
 	} else {
 		bat (/call mvn surefire-report:report/)
-		// junit '**/target/surefire-reports/TEST-.xml'}
+
 }
 
