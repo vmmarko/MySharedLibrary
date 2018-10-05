91473d53d5d5e428ebc281f1f26f3215c4120caa
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 17:14:17 +0200

    node()

--- vars/myPreparation.groovy
+++ vars/myPreparation.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 import common.*
 
-node {
-def call() {
-	echo "Preparation my !"
+node() {
+	def call() {
+		echo "Preparation my !"
 		
-	// Get some code from a GitHub repository
-	git 'https://github.com/vmmarko/Azure-training.git'
+		// Get some code from a GitHub repository
+		git 'https://github.com/vmmarko/Azure-training.git'
 		
-}
+	}
 }
