b8e289410057ecd5f384c9e5cbae5b78f23149a4
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 18:50:56 +0200

    prep

--- vars/myPreparation.groovy
+++ vars/myPreparation.groovy
@@ -1,20 +1,23 @@
 #!/usr/bin/env groovy
 import common.*
 
-node {
-	stage ('Preparation') {
-		echo "Preparation my !"
+def myPreparation(){
+	node {
+		stage ('Preparation') {
+			echo "Preparation my !"
 
-		def mvnHome
+			def mvnHome
 
-		// Get some code from a GitHub repository
-		git 'https://github.com/vmmarko/Azure-training.git'
-/*
-		withMaven(
-				maven: 'M3',
-				mavenSettingsConfig: 'my-maven-settings',
-				mavenLocalRepo: '.repository')
-		{  sh "mvn clean install" }
+			// Get some code from a GitHub repository
+			git 'https://github.com/vmmarko/Azure-training.git'
+			/*
+			 withMaven(
+			 maven: 'M3',
+			 mavenSettingsConfig: 'my-maven-settings',
+			 mavenLocalRepo: '.repository')
+			 {  sh "mvn clean install" }
+			 }
+			 */	
+		}
 	}
-*/	
 }
