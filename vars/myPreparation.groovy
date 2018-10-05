c16e3a08cc147350078751316141ffb022268796
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 16:43:53 +0200

    mvn

--- vars/myPreparation.groovy
+++ vars/myPreparation.groovy
@@ -4,9 +4,7 @@ import common.*
 node {
 def call() {
 	echo "Preparation my !"
-	
-	def mvnHome
-	
+		
 	// Get some code from a GitHub repository
 	git 'https://github.com/vmmarko/Azure-training.git'
 		
