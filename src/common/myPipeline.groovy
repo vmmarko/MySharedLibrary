1819c6dafa315d0ff5035bbaffa0bd4fefd05f52
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 15:17:43 +0200

    myPipeline

--- src/common/myPipeline.groovy
+++ src/common/myPipeline.groovy
@@ -2,9 +2,48 @@
 
 package common
 
-class myPipeline {
-
+def preparation(){
+	// Get some code from a GitHub repository
+	git 'https://github.com/vmmarko/Azure-training.git'
 }
+
+
+def compile(){
+	if (isUnix()){
+		sh '$MAVEN_HOME/mvn compile'
+	} else {
+		bat(/call mvn clean compile/)
+	}
+}
+
+
+def build() {
+	//mvn clean deploy -U
+	if (isUnix()) {
+		sh '$MAVEN_HOME/mvn package'
+	 } else {
+		bat(/call mvn package/)
+	 }
+}
+
+def test() {
+	//sh "/usr/local/bin/${name}"
+	if (isUnix()){
+		sh '$MAVEN_HOME/mvn surefire-report:report'
+	} else {
+		bat (/call mvn surefire-report:report/)
+	}
+}
+
+def deploy(env,app) {
+	if (isUnix()){
+		sh '$MAVEN_HOME/mvn tomcat7:redeploy'
+	} else {
+		bat (/call mvn tomcat7:redeploy/)
+	}
+}
+
+
 
 /*
  * def build() {
@@ -21,5 +60,10 @@ class myPipeline {
 	  ParameterKey=Env,ParameterValue=${env}
 	...(rest of the params go here)
   }
+
+class myPipeline {
+
+}
+
 
   */
