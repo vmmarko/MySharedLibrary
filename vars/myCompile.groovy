4a66bf3d780eff020e8142518bb8ce504bc4e885
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 17:44:47 +0200

    compile

--- vars/myCompile.groovy
+++ vars/myCompile.groovy
@@ -1,13 +1,18 @@
 #!/usr/bin/env groovy
+//import common.*
 
-def call() {
-	echo "Compile !"
-	
-	def mvnHome
-	
-	if (isUnix()){
-		sh '$MAVEN_HOME/mvn compile'
-	} else {
-		bat(/call mvn clean compile/)
+node {
+	stage ('Compile') {
+		echo "Compile !"
+
+		def mvnHome
+
+		if (isUnix()){
+			sh '$MAVEN_HOME/mvn compile'
+		} else {
+			bat(/call mvn clean compile/)
+		}
 	}
 }
+
+
