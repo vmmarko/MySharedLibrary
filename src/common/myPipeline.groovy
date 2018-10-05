53e50f0588564dd7a78b79d6897b83fd24d75952
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 13:29:08 +0200

    vars

--- src/common/myPipeline.groovy
+++ src/common/myPipeline.groovy
@@ -1,5 +1,21 @@
+#!/usr/bin/groovy
+
 package common
 
 class myPipeline {
 
 }
+
+def build() {
+	mvn clean deploy -U
+  }
+  def test(name) {
+	sh "/usr/local/bin/${name}"
+  }
+  def deploy(env,app) {
+	aws cloudformation create-stack \
+	--stack-name ${env}_{app)
+	--parameters \
+	  ParameterKey=Env,ParameterValue=${env}
+	...(rest of the params go here)
+  }
