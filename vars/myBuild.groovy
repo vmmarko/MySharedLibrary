d7d30571134af87324ac302825614b8ea1fd9f05
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-10-05 18:24:44 +0200

    prep

--- vars/myBuild.groovy
+++ vars/myBuild.groovy
@@ -23,19 +23,6 @@ node {
 //}
 /*
 
-withMaven(
-			// Maven installation declared in the Jenkins "Global Tool Configuration"
-			maven: 'M3',
-			// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-			mavenSettingsConfig: 'my-maven-settings',
-			mavenLocalRepo: '.repository') {
-	 
-		  // Run the maven build
-		  sh "mvn clean install"
-	 
-		}
-		
 		
 		
 def call() {
--- vars/myPreparation.groovy
+++ vars/myPreparation.groovy
@@ -4,12 +4,16 @@ import common.*
 node {
 	stage ('Preparation') {
 		echo "Preparation my !"
-		
+
 		def mvnHome
-		
+
 		// Get some code from a GitHub repository
 		git 'https://github.com/vmmarko/Azure-training.git'
-		
-		
+
+		withMaven(
+				maven: 'M3',
+				mavenSettingsConfig: 'my-maven-settings',
+				mavenLocalRepo: '.repository')
+		{  sh "mvn clean install" }
 	}
 }
